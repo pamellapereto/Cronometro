47ca4c27ea6f5fa1f379a1863c944c533fb10493
pamellapereto <pamellapereto@gmail.com>
2023-04-13 11:03:11 -0300

    Primeira versão

---
+++ cronometro.py
@@ -0,0 +1,30 @@
+import time
+
+
+def contagem_regressiva(segundos):
+
+    while segundos > 0:
+        
+        minutes = int(segundos / 60)     
+
+        seconds = int(segundos % 60)
+
+        cronometro = f'{minutes} : {seconds}'
+
+        print(cronometro)#, end='\r')
+
+        time.sleep(1)
+    
+        segundos -= 1
+    
+    print("Tempo esgotado!")
+
+
+
+
+segundos = input("Digite o tempo em segundos: ")
+
+contagem_regressiva(int(segundos))
+
+
+
